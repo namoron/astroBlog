84e81d76e938e2b739e0d67f56e10fa10144314f
niimi <213shirokumals530hl@gmail.com>
2024-12-24 16:41:53 +0900

    partytown を astro.config.mjs に追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import remarkCodeTitles from "remark-flexible-code-titles";
 
